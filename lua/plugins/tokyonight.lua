cd2f077125f1022ef58aa79415444e1dca78c084
Junior Saldanha <umsaldanha@gmail.com>
2023-11-18 01:42:33 -0500

    first commit

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    transparent = true,
+    theme = "night",
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
